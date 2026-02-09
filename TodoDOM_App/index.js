51ed6bed1637f2ab3731dc8abf0fe54cff2abda9
Priyanshu Pandey <priyanshupandey2007@gmail.com>
2026-02-09 11:24:36 +0530

    priyanshupandey2007

--- TodoDOM_App/index.js
+++ TodoDOM_App/index.js
@@ -1,38 +1,79 @@
-// step-1 catch/select the form tag and attach event 
-document.querySelector("form").addEventListener("submit",getData );
- function getData(e){
-    e.preventDefault(); // to prevent the default behaviour of form(submission and page reload)
+// step -1 select form
 
+// let todoArr;
 
-    let taskName = document.querySelector("#task").value;
-    let taskPriority = document.querySelector("#priority").value;
+let todoArr = JSON.parse(localStorage.getItem("todo")) || [];
+let favArr = JSON.parse(localStorage.getItem("fav")) || [];
 
-    console.log(taskName,taskPriority);
+document.querySelector("form").addEventListener("submit", getData);
+displayTable(todoArr);
 
-    let taskObj = {
-        taskName,
-        taskPriority,
-    };
-    console.log(taskObj);
-    displayTask(taskObj);
- }
+// if(JSON.parse(localStorage.getItem("todo"))==null){
+//   todoArr = []
+// } else{
+//   todoArr = JSON.parse(localStorage.getItem("todo"))
+// }
 
- function displayTask(taskObj){
+// step -2 Get the data from form
 
- const row = document.createElement("tr");
+function getData(e) {
+  e.preventDefault();
+  const taskName = document.querySelector("#task").value;
+  const taskPriority = document.querySelector("#priority").value;
 
- const td1 = document.createElement("td");
- td1.innerText = taskObj.taskName;
+  //   console.log(taskName, taskPriority);
 
- const td2 = document.createElement("td");  
-    td2.innerText = taskObj.taskPriority;
+  //   let taskObj = {
+  //     taskName:taskName,
+  //     taskPriority:taskPriority
+  //   }
 
-    const td3 = document.createElement("td");
-    td3.innerText = "Add";
+  let taskObj = {
+    taskName,
+    taskPriority,
+  };
 
-    row.append(td1,td2,td3);
-    
+  todoArr.push(taskObj);
 
-    document.querySelector("tbody").appendChild(row);
+  localStorage.setItem("todo", JSON.stringify(todoArr));
+  console.log(todoArr);
 
- }
+  displayTable(todoArr); // [{},{},{}]
+}
+//   <tr>
+//       <td>aa</td>
+//       <td>High</td>
+//       <td>Add</td>
+//      </tr>
+
+function displayTable(arr) {
+  document.querySelector("tbody").innerText = "";
+  arr.forEach((el) => {
+    let row = document.createElement("tr");
+
+    let td1 = document.createElement("td");
+    td1.innerText = el.taskName;
+
+    let td2 = document.createElement("td");
+    td2.innerText = el.taskPriority;
+
+    if (td2.innerText == "High") {
+      td2.style.color = "red";
+    } else {
+      td2.style.color = "green";
+    }
+
+    let td3 = document.createElement("button");
+    td3.innerText = "add";
+
+    td3.addEventListener("click", function () {
+      favArr.push(el);
+      localStorage.setItem("fav", JSON.stringify(favArr));
+      alert("Todo added to Fav");
+    });
+
+    row.append(td1, td2, td3);
+
+    document.querySelector("tbody").append(row);
+  });
+}
