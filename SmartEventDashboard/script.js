ca954485fde11e681a053425061df9322375a5d0
Priyanshu Pandey <priyanshupandey2007@gmail.com>
2026-02-12 09:27:08 +0530

    priyanshupandey2007

---
+++ SmartEventDashboard/script.js
@@ -0,0 +1,39 @@
+const form=document.querySelector(".form")
+const eventCards=document.querySelector(".cards")
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    console.log(eventTitle.value)
+    console.log(eventDate.value)
+    console.log(category.value)
+    console.log(description.value)
+
+    let title=eventTitle.value
+    let date=eventDate.value
+    let cat=category.value
+    let desc=description.value
+
+    const card=document.createElement("div")
+    card.classList.add(`card`)
+        card.innerHTML=`
+        <h3>${title}</h3>
+        <p>${date}</p>
+        <button>${cat}</button>
+        <p>${desc}</p>
+        <div class="dlt">x</div>
+        `
+    eventCards.appendChild(card)
+    const dlt=card.querySelector(".dlt")
+    dlt.addEventListener("click",()=>{
+        card.remove()
+    })
+})
+// document.addEventListener("keydown",(e)=>{
+//      console.log(e.key)
+// })
+
+document.querySelector(".clearAll").addEventListener("click",()=>{
+    document.querySelectorAll(".card").forEach((card)=>{
+        card.remove()
+    })
+})
