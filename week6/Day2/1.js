92999608217c04fdc09eaa2b510b57efc11229d9
priyanshupandey2007 <priyanshupandey2864@gmail.com>
2026-02-26 10:30:17 +0530

    25-02-26

---
+++ week6/Day2/1.js
@@ -0,0 +1,45 @@
+// promise is most important for mid term 
+
+
+
+// 7. clousers ->  function + its lexical scope of varibale 
+//  Encapsulation in js is achived by clousers 
+// function outer(){
+// let count = 0;
+//   function inner (){
+//     count++;
+//     console.log(count);
+// }
+// }
+
+// let ans = outer();
+// ans()
+// ans()
+// ans()
+
+
+//  8.Curring 
+
+// function add(a){
+//      return function(b){
+//        return   function(c){
+//             return  function(d){
+//              return    a+b+c+d;
+//             }
+//         }
+//     }
+// }
+
+// let ans = add(1)(2)(3)(4);
+// console.log(ans);
+
+// arrow function
+// const add = (a)=>(b)=>(c)=>(d)=>a+b+c+d;
+ 
+// let ans = add(1)(2)(3)(4);
+// console.log(ans);
+
+// promises -> A promises is an object that may produce a single value some time in the future: either a resolved value or a reason that it's not resolved that it's not resolved 
+// 3 states of promises :   pending , fullfilled , rejected
+
+ 
--- week6/day1/1.js
+++ week6/day1/1.js
@@ -1,67 +1,203 @@
-// asymcronous programming
+// Asynchronous Programming (Welcome to Advanced concept of Js)
 
-// language -->  miduim to comunicate
-// programming language ---> set of instruction that we give to computer/m/c for genrating various kind of output
+// Language - > medium to communicate
+// Programming Language -> set of instruction that we give to the computer/m/c for
+// generating various kind of output
 
 // 2 type
-// high level language progg language ---> javascript, python
-// low level language ----> M/c code, binary language
+    // High level Progg language - javascript, python
+    // Low level Progg language - M/c code, binary language
 
-// compiled progg language ------> cinvert high to low in 1 go
-// Interpreted progg---> convcert high to low in line by line and exicuted imediately
+// compilation process ->High level code is converted into low level code
 
-// 1. intreprated language
+    // compiled Progg language -> convert high to low in 1 go
+    // Interpreted Progg  -> convert high to low in line by line and executed immediately 
+
+
+
+
+
+
+
+
+
+// 1. Interpreted Progg language
 
 // var name = "ram";
 // console.log(name, typeof name);
 
-// name= true;
+// name = true;
 // console.log(name, typeof name);
+
 
 // name = 10;
+// console.log(name,typeof name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//2. Dynamically Type Programming Language
+
+
+// var name = "ram";
 // console.log(name, typeof name);
 
-// 3.hosting // machanism in which var keyword se declare kiye gaye to aaap usko pehle bhi use kr sakte ho uske declare hone se pehle bhi
-// var ---> undefined
-//  regular fn -----> fn ko memeory  allot ho jata hai
+// name = true;
+// console.log(name, typeof name);
 
-// let x;  // devlaration
-// x=10; // assignment
 
-// console.log(name); // undefined
-
-// var name = "hello"
-
-// console.log(name); // hello
+// name = 10;
+// console.log(name,typeof name);
 
 
 
 
 
-// 4. lexical scope in JS  -> visibility / accessibility 
 
 
-// function hello() {
-//     var name="priyanshu";
-//     let age= 25;
 
-//     console.log(name);
-//     console.log(age);
 
-//     if (true){
-//         name="pandey";
-//         age=21;
-//         console.log(name);
-//         console.log(age);
-//     }
-// console.log(name);
-// console.log(age);
+
+
+
+
+
+
+//3. Hoisting - 
+
+// var -> undefined
+// regular fn. -> fn ko memeory allot ho jata hai
+
+// let x; // decleration
+// x = 10; // assignment
+
+// let x  = 20;
+
+// console.log(x);
+
+
+// console.log(name); // err
+
+// var name = "hello";
+
+// console.log(name); //hello
+
+// console.log(age); // 25
+
+// let age = 25;
+
+// console.log(age); //25
+
+// hello() // err, un
+
+// function hello(){
+//     console.log("hello");
+    
 // }
 
+// hello() // hello
+
+
+// add(); // un
+
+
+// var add = ()=>{
+//     console.log("hello");
+    
+// }
+
+
+// add(); // he
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+//4. Lexical Scope in JS - > visibility/ accessibility
+
+
 // hello()
+// function hello(){
+//     console.log(name); //err un
+//     console.log("error"); // err
+
+//     var name = "ram";
+//     let age = 25;
+    
+//     console.log(name); // ram
+//     console.log(age); // 25
+
+//     if(true){
+//         name = "shyam";
+//         age = 21;
+
+//     console.log(name); // shy
+//     console.log(age);  // 21
+//     }
+    
+//     console.log(name); // shy
+//     console.log(age); // 21
+// }
 
 
-// 5.Execution 
+// hello()
+// function hello(){
+//     console.log(name); // un
+//     console.log("error"); // error bcz of hoisting
+
+//     if(true){
+//     var   name = "shyam";
+//     let  age = 21;
+
+//     console.log(name); // shy
+//     console.log(age);  // 21
+//     }
+    
+//     console.log(name); // shy
+//     console.log(age); // error bcz of lexical scope
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+//5.  Execution Context
+
 // console.log(one());
 // console.log("start");
 
@@ -71,76 +207,114 @@
 
 //     three();
 //     console.log(name);
-//     return "shyam"
+//     return "shyam"    
 // }
 
 // console.log("mid");
 
 // function three(){
-// return " hello developers"
+//     return "hello developers"
 // }
-
 // function two(){
 //     console.log("error");
 //     let age = 21;
-//     console.log(three());
+//         console.log(three());
 //     console.log(age);
-
+    
 // }
 // console.log(two());
 // console.log("end");
 // one()
 
-// 6. Timer 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 5. Timer 
+
 // console.log("start");
 
+
 // setTimeout(() => {
-//     console.log("hello");
-// }, 2000);
+//     console.log("mid");
+    
+// }, 5000);
 
 // console.log("end");
 
+// setInterval(()=>{
+//     console.log("chup ho jao varna ...🔫");
+    
+// },3000)
 
-// setInterval(() => {
-//     console.log("00:00 🚀");
-// }, 2000);
 
 
-// 7. Event loop
-console.log("start");
 
-setInterval(() => {
-    three()
-},10000)
 
-function one(){
-    console.log(name);
-    var name = "priyanshu";
 
-    return name;
-    console.log(three());
-}
 
-setTimeout(() => {
-    console.log(one());
-}, 3000);
 
-function two(){
-    console.log(age);
-    var age = 25;
-    console.log(three());
 
-    return age;
 
-}
-function three(){
-    console.log("abb kya hi bolu ... ");
 
-}
 
-console.log("mid");
-setTimeout(() => {
-    console.log(two());
-}, 5000);
 
-console.log("end")
+
+
+
+
+
+//6. Event Loop
+// console.log("start");
+
+// setInterval(()=>{
+//     three()
+// },10000)
+
+// function one(){
+//     console.log(name);
+//     var name = "love";
+
+//     return name;
+//     console.log(three());
+// }
+
+// setTimeout(()=>{
+//     console.log(one());
+// },3000)
+
+// function two(){
+//     console.log(age);
+//     var age = 25;
+//     console.log(three());
+
+//     return age;
+// }
+
+// function three(){
+//     console.log("abb kyy hi bole...");
+// }
+
+// console.log("mid");
+
+// setTimeout(()=>{
+//     console.log(two());
+// },5000)
+
+// console.log("end");
